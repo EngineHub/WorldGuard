b01b67f64c122b028a533481b74e523232236a3f
sk89q <the.sk89q@gmail.com>
2011-01-17 00:16:02 -0800

    Cleaned up code.

--- src/com/sk89q/worldguard/blacklist/BlacklistEntry.java
+++ src/com/sk89q/worldguard/blacklist/BlacklistEntry.java
@@ -21,8 +21,6 @@ package com.sk89q.worldguard.blacklist;
 
 import java.util.Set;
 import java.util.HashSet;
-import java.util.Map;
-import java.util.HashMap;
 import org.bukkit.ChatColor;
 import com.sk89q.worldedit.blocks.ItemType;
 import com.sk89q.worldguard.LocalPlayer;
--- src/com/sk89q/worldguard/bukkit/WorldGuardPlugin.java
+++ src/com/sk89q/worldguard/bukkit/WorldGuardPlugin.java
@@ -24,7 +24,6 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
