a473f472354d201cc797abae9274280d60a91e09
Redecouverte <michael.heerklotz@web.de>
2011-02-26 01:18:50 +0100

    /tpregion now checks for flag rights

--- src/com/sk89q/worldguard/bukkit/commands/CommandTpRegion.java
+++ src/com/sk89q/worldguard/bukkit/commands/CommandTpRegion.java
@@ -3,6 +3,8 @@
  */
 package com.sk89q.worldguard.bukkit.commands;
 
+import com.sk89q.worldguard.LocalPlayer;
+import com.sk89q.worldguard.bukkit.BukkitPlayer;
 import com.sk89q.worldguard.bukkit.WorldGuardConfiguration;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -39,6 +41,25 @@ public class CommandTpRegion extends WgCommand {
         RegionManager mgr = cfg.getWorldGuardPlugin().getGlobalRegionManager().getRegionManager(player.getWorld().getName());
         ProtectedRegion region = mgr.getRegion(id);
         if (region != null) {
+
+            String flagright = "all";
+            if (spawn) {
+                flagright = region.getFlags().getFlag("spawn", "settings");
+            } else {
+                flagright = region.getFlags().getFlag("teleport", "settings");
+            }
+
+            LocalPlayer lPlayer = BukkitPlayer.wrapPlayer(cfg, player);
+            if (flagright.equals("owner")) {
+                if (!region.isOwner(lPlayer)) {
+                    cfg.checkPermission(player, "tpregion.override");
+                }
+            } else if (flagright.equals("meber")) {
+                if (!region.isMember(lPlayer)) {
+                    cfg.checkPermission(player, "tpregion.override");
+                }
+            }
+
             Location location = null;
 
             if (spawn) {
