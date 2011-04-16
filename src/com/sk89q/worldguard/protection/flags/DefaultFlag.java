e5d05993577becbb0f262dbd4dbb566e3d1d6962
Wizjany <wizjany@gmail.com>
2011-04-15 20:26:24 -0400

    Added a lot of missing things to player listener.

--- src/com/sk89q/worldguard/protection/flags/DefaultFlag.java
+++ src/com/sk89q/worldguard/protection/flags/DefaultFlag.java
@@ -29,8 +29,9 @@ public final class DefaultFlag {
     public static final StateFlag BUILD = new StateFlag("build", 'b', true);
     public static final StateFlag PVP = new StateFlag("pvp", 'p', true);
     public static final StateFlag MOB_DAMAGE = new StateFlag("mob-damage", 'm', true);
-    public static final StateFlag MOB_SPAWNING = new StateFlag("mob-spawning", 'M', true);
+    public static final StateFlag MOB_SPAWNING = new StateFlag("mob-spawning", true);
     public static final StateFlag CREEPER_EXPLOSION = new StateFlag("creeper-explosion", 'c', true);
+    public static final StateFlag SLEEP = new StateFlag("sleep", true);
     public static final StateFlag TNT = new StateFlag("tnt", 't', true);
     public static final StateFlag LIGHTER = new StateFlag("lighter", 'l', true);
     public static final StateFlag FIRE_SPREAD = new StateFlag("fire-spread", 'f', true);
@@ -55,7 +56,7 @@ public final class DefaultFlag {
     public static final DoubleFlag PRICE = new DoubleFlag("price");
     
     public static final Flag<?>[] flagsList = new Flag<?>[] {
-        PASSTHROUGH, BUILD, PVP, MOB_DAMAGE, MOB_SPAWNING, CREEPER_EXPLOSION,
+        PASSTHROUGH, BUILD, PVP, MOB_DAMAGE, MOB_SPAWNING, CREEPER_EXPLOSION, SLEEP,
         TNT, LIGHTER, FIRE_SPREAD, LAVA_FIRE, CHEST_ACCESS, WATER_FLOW, LAVA_FLOW,
         USE, PLACE_VEHICLE, GREET_MESSAGE, FAREWELL_MESSAGE, NOTIFY_GREET,
         NOTIFY_FAREWELL, DENY_SPAWN, HEAL_DELAY, HEAL_AMOUNT, TELE_LOC,
