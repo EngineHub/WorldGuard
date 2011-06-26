0fa43ee287be22b972ef8dadd3d7a35327aca9ae
sk89q <the.sk89q@gmail.com>
2011-06-26 15:11:05 -0700

    Added a warning about modifying region database files manually.

--- src/main/java/com/sk89q/worldguard/util/yaml/Configuration.java
+++ src/main/java/com/sk89q/worldguard/util/yaml/Configuration.java
@@ -70,6 +70,7 @@ import org.yaml.snakeyaml.representer.Representer;
 public class Configuration extends ConfigurationNode {
     private Yaml yaml;
     private File file;
+    private String header = null;
     
     public Configuration(File file) {
         super(new HashMap<String, Object>());
@@ -105,31 +106,79 @@ public class Configuration extends ConfigurationNode {
             }
         }
     }
-    
+
     /**
-     * Saves the configuration to disk.  
-     *  
-     * @throws IOException 
+     * Set the header for the file as a series of lines that are terminated
+     * by a new line sequence.
+     *
+     * @param headerLines header lines to prepend
      */
-    public void save() throws IOException {
+    public void setHeader(String... headerLines) {
+        StringBuilder header = new StringBuilder();
+
+        for (String line : headerLines) {
+            if (header.length() > 0) {
+                header.append("\r\n");
+            }
+            header.append(line);
+        }
+
+        setHeader(header.toString());
+    }
+
+    /**
+     * Set the header for the file. A header can be provided to prepend the
+     * YAML data output on configuration save. The header is
+     * printed raw and so must be manually commented if used. A new line will
+     * be appended after the header, however, if a header is provided.
+     *
+     * @param header header to prepend
+     */
+    public void setHeader(String header) {
+        this.header = header;
+    }
+
+    /**
+     * Return the set header.
+     *
+     * @return
+     */
+    public String getHeader() {
+        return header;
+    }
+
+    /**
+     * Saves the configuration to disk. All errors are clobbered.
+     *
+     * @return true if it was successful
+     */
+    public boolean save() {
         FileOutputStream stream = null;
-        
+
         File parent = file.getParentFile();
+
         if (parent != null) {
             parent.mkdirs();
         }
-        
+
         try {
             stream = new FileOutputStream(file);
-            yaml.dump(root, new OutputStreamWriter(stream, "UTF-8"));
-        } finally {
+            OutputStreamWriter writer = new OutputStreamWriter(stream, "UTF-8");
+            if (header != null) {
+                writer.append(header);
+                writer.append("\r\n");
+            }
+            yaml.dump(root, writer);
+            return true;
+        } catch (IOException e) {} finally {
             try {
                 if (stream != null) {
                     stream.close();
                 }
-            } catch (IOException e) {
-            }
+            } catch (IOException e) {}
         }
+
+        return false;
     }
     
     @SuppressWarnings("unchecked")
