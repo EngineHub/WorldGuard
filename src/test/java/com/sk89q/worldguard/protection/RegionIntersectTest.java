544e5c2945ef18b31a3f00c1446bdd2d26ba7663
Eric Puidokas <eric@puidokas.com>
2011-10-17 19:54:36 -0700

    Fixed ProtectedCuboidRegion->ProtectedCuboidRegion intersection checking

--- src/test/java/com/sk89q/worldguard/protection/RegionIntersectTest.java
+++ src/test/java/com/sk89q/worldguard/protection/RegionIntersectTest.java
@@ -3,7 +3,6 @@ package com.sk89q.worldguard.protection;
 import com.sk89q.worldedit.BlockVector;
 import com.sk89q.worldguard.protection.regions.ProtectedCuboidRegion;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
-import org.junit.Ignore;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -13,7 +12,6 @@ import java.util.List;
 public class RegionIntersectTest {
 
     @Test
-    @Ignore
     public void testCuboidGetIntersectingRegions() {
         ProtectedRegion region = new ProtectedCuboidRegion("square",
                 new BlockVector(100, 40, 0), new BlockVector(140, 128, 40));
