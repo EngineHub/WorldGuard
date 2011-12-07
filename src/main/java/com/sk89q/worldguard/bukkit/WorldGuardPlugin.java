b1256eedcf38fbad1dda0636255730409960f86c
zml2008 <zach@zachsthings.com>
2011-12-07 07:33:53 -0800

    Readded the legacy console sender.

--- src/main/java/com/sk89q/worldguard/bukkit/WorldGuardPlugin.java
+++ src/main/java/com/sk89q/worldguard/bukkit/WorldGuardPlugin.java
@@ -569,7 +569,11 @@ public class WorldGuardPlugin extends JavaPlugin {
         if (filter.equalsIgnoreCase("#console")
                 || filter.equalsIgnoreCase("*console*")
                 || filter.equalsIgnoreCase("!")) {
-            return getServer().getConsoleSender();
+            try {
+                return getServer().getConsoleSender();
+            } catch (Throwable t) {
+                return new LegacyConsoleSender(getServer());
+            }
         }
         
         return matchSinglePlayer(sender, filter);
