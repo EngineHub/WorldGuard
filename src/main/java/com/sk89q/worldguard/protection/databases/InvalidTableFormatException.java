12e94d8fbb00d578e034c58b2eed6f0614d572b4
Nicholas Steicke <nicholas.steicke@narthollis.net>
2012-03-27 01:57:47 +1030

    Added a generic exception for Fatal errors when loading configuration

--- src/main/java/com/sk89q/worldguard/protection/databases/InvalidTableFormatException.java
+++ src/main/java/com/sk89q/worldguard/protection/databases/InvalidTableFormatException.java
@@ -1,6 +1,8 @@
 package com.sk89q.worldguard.protection.databases;
 
-public class InvalidTableFormatException extends RuntimeException {
+import com.sk89q.worldguard.util.FatalConfigurationLoadingException;
+
+public class InvalidTableFormatException extends FatalConfigurationLoadingException {
 	private static final long serialVersionUID = 1L;
 
 	protected String updateFile;
