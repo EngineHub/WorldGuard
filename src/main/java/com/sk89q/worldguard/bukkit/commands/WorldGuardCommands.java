55b41b3762efc86babf72b28060b52641e10b04f
Dilandau <mhonline@sheeris.com>
2012-11-07 09:46:38 +0100

    Clean cache in WGBukkit when reloaded

--- src/main/java/com/sk89q/worldguard/bukkit/commands/WorldGuardCommands.java
+++ src/main/java/com/sk89q/worldguard/bukkit/commands/WorldGuardCommands.java
@@ -35,6 +35,7 @@ import com.sk89q.minecraft.util.commands.CommandPermissions;
 import com.sk89q.worldguard.bukkit.LoggerToChatHandler;
 import com.sk89q.worldguard.bukkit.ReportWriter;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.protection.flags.WGBukkit;
 import com.sk89q.worldguard.util.PastebinPoster;
 import com.sk89q.worldguard.util.PastebinPoster.PasteCallback;
 
@@ -72,6 +73,7 @@ public class WorldGuardCommands {
             plugin.getGlobalRegionManager().unload();
             plugin.getGlobalStateManager().load();
             plugin.getGlobalRegionManager().preload();
+            WGBukkit.cleanCache();
             sender.sendMessage("WorldGuard configuration reloaded.");
         } catch (Throwable t) {
             sender.sendMessage("Error while reloading: "
