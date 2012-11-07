463b1a7e0851c3a482d718ddf5a97fcc2b8d72e0
Dilandau <mhonline@sheeris.com>
2012-11-07 10:02:34 +0100

    Fix package name.

--- src/main/java/com/sk89q/worldguard/bukkit/WGBukkit.java
+++ src/main/java/com/sk89q/worldguard/bukkit/WGBukkit.java
@@ -16,7 +16,7 @@
  * this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-package com.sk89q.worldguard.protection.flags;
+package com.sk89q.worldguard.bukkit;
 
 import org.bukkit.Bukkit;
 import org.bukkit.World;
