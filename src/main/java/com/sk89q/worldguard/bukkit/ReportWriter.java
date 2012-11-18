644d6ee51245311261c2ed91e2f04683850f128d
sk89q <the.sk89q@gmail.com>
2012-11-17 16:35:48 -0800

    Commented out regions-related code in ReportWriter as it is no longer valid.

--- src/main/java/com/sk89q/worldguard/bukkit/ReportWriter.java
+++ src/main/java/com/sk89q/worldguard/bukkit/ReportWriter.java
@@ -35,22 +35,17 @@ import org.bukkit.World;
 import org.bukkit.entity.Entity;
 import org.bukkit.plugin.Plugin;
 
-import com.sk89q.worldguard.region.Region;
 import com.sk89q.worldguard.region.RegionManager;
-import com.sk89q.worldguard.region.flags.DefaultFlag;
-import com.sk89q.worldguard.region.flags.Flag;
-import com.sk89q.worldguard.region.flags.StateFlag;
-import com.sk89q.worldguard.region.indices.RegionIndex;
 import com.sk89q.worldguard.util.LogListBlock;
 
 public class ReportWriter {
 
     private static final SimpleDateFormat dateFmt =
         new SimpleDateFormat("yyyy-MM-dd kk:mm Z");
-    
+
     private Date date = new Date();
     private StringBuilder output = new StringBuilder();
-    
+
     public ReportWriter(WorldGuardPlugin plugin) {
         appendReportHeader(plugin);
         appendServerInformation(plugin.getServer());
@@ -63,38 +58,38 @@ public class ReportWriter {
         appendln("END OF REPORT");
         appendln();
     }
-    
+
     protected static String repeat(String str, int n) {
         if(str == null) {
             return null;
         }
-        
+
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < n; i++) {
             sb.append(str);
         }
-        
+
         return sb.toString();
     }
-    
+
     protected void appendln(String text) {
         output.append(text);
         output.append("\r\n");
     }
-    
+
     protected void appendln(String text, Object ... args) {
         output.append(String.format(text, args));
         output.append("\r\n");
     }
-    
+
     protected void append(LogListBlock log) {
         output.append(log.toString());
     }
-    
+
     protected void appendln() {
         output.append("\r\n");
     }
-    
+
     protected void appendHeader(String text) {
         String rule = repeat("-", text.length());
         output.append(rule);
@@ -104,7 +99,7 @@ public class ReportWriter {
         output.append("\r\n");
         appendln();
     }
-    
+
     private void appendReportHeader(WorldGuardPlugin plugin) {
         appendln("WorldGuard Configuration Report");
         appendln("Generated " + dateFmt.format(date));
@@ -112,13 +107,13 @@ public class ReportWriter {
         appendln("Version: " + plugin.getDescription().getVersion());
         appendln();
     }
-    
+
     private void appendGlobalConfiguration(ConfigurationManager config) {
         appendHeader("Global Configuration");
-        
+
         LogListBlock log = new LogListBlock();
         LogListBlock configLog = log.putChild("Configuration");
-        
+
         Class<? extends ConfigurationManager> cls = config.getClass();
         for (Field field : cls.getFields()) {
             try {
@@ -130,16 +125,16 @@ public class ReportWriter {
             } catch (IllegalAccessException ignore) {
             }
         }
-        
+
         append(log);
         appendln();
     }
-    
+
     private void appendServerInformation(Server server) {
         appendHeader("Server Information");
-        
+
         LogListBlock log = new LogListBlock();
-        
+
         Runtime runtime = Runtime.getRuntime();
 
         log.put("Java", "%s %s (%s)",
@@ -164,23 +159,23 @@ public class ReportWriter {
         append(log);
         appendln();
     }
-    
+
     private void appendPluginInformation(Plugin[] plugins) {
         appendHeader("Plugins (" + plugins.length + ")");
-        
+
         LogListBlock log = new LogListBlock();
-        
+
         for (Plugin plugin : plugins) {
             log.put(plugin.getDescription().getName(), plugin.getDescription().getVersion());
         }
 
         append(log);
         appendln();
-        
+
         /*appendHeader("Plugin Information");
-        
+
         log = new LogListBlock();
-        
+
         for (Plugin plugin : plugins) {
             log.putChild(plugin.getDescription().getName())
                 .put("Data folder", plugin.getDataFolder())
@@ -191,20 +186,20 @@ public class ReportWriter {
         append(log);
         appendln();*/
     }
-    
+
     private void appendWorldInformation(List<World> worlds) {
         appendHeader("Worlds");
-        
+
         LogListBlock log = new LogListBlock();
-        
+
         int i = 0;
         for (World world : worlds) {
             int loadedChunkCount = world.getLoadedChunks().length;
-            
+
             LogListBlock worldLog = log.putChild(world.getName() + " (" +  i + ")");
             LogListBlock infoLog = worldLog.putChild("Information");
             LogListBlock entitiesLog = worldLog.putChild("Entities");
-            
+
             infoLog.put("Seed", world.getSeed());
             infoLog.put("Environment", world.getEnvironment().toString());
             infoLog.put("Player count", world.getPlayers().size());
@@ -212,21 +207,21 @@ public class ReportWriter {
             infoLog.put("Loaded chunk count", loadedChunkCount);
             infoLog.put("Spawn location", world.getSpawnLocation());
             infoLog.put("Raw time", world.getFullTime());
-            
+
             Map<Class<? extends Entity>, Integer> entityCounts =
                     new HashMap<Class<? extends Entity>, Integer>();
-            
+
             // Collect entities
             for (Entity entity : world.getEntities()) {
                 Class<? extends Entity> cls = entity.getClass();
-                
+
                 if (entityCounts.containsKey(cls)) {
                     entityCounts.put(cls, entityCounts.get(cls) + 1);
                 } else {
                     entityCounts.put(cls, 1);
                 }
             }
-            
+
             // Print entities
             for (Map.Entry<Class<? extends Entity>, Integer> entry
                     : entityCounts.entrySet()) {
@@ -235,18 +230,18 @@ public class ReportWriter {
                         entry.getValue(),
                         (float) (entry.getValue() / (double) loadedChunkCount));
             }
-            
+
             i++;
         }
 
         append(log);
         appendln();
     }
-    
+
     private void appendWorldConfigurations(WorldGuardPlugin plugin, List<World> worlds,
             RegionManager regionMgr, ConfigurationManager mgr) {
         appendHeader("World Configurations");
-        
+
         LogListBlock log = new LogListBlock();
 
         int i = 0;
@@ -264,9 +259,9 @@ public class ReportWriter {
                     + world.getName() + "/blacklist.txt")).getAbsoluteFile());
             infoLog.put("Regions file", (new File(plugin.getDataFolder(), "worlds/"
                     + world.getName() + "/regions.yml")).getAbsoluteFile());
-            
+
             WorldConfiguration config = mgr.get(world);
-            
+
             Class<? extends WorldConfiguration> cls = config.getClass();
             for (Field field : cls.getFields()) {
                 try {
@@ -277,7 +272,7 @@ public class ReportWriter {
                 } catch (IllegalAccessException ignore) {
                 }
             }
-            
+
             if (config.getBlacklist() == null) {
                 blacklistLog.put("State", "DISABLED");
             } else {
@@ -288,12 +283,12 @@ public class ReportWriter {
                         config.getBlacklist().isWhitelist());
             }
 
-            RegionIndex worldRegions = regionMgr.get(world);
+            /*RegionIndex worldRegions = regionMgr.get(world);
 
             regionsLog.put("Type", worldRegions.getClass().getCanonicalName());
             regionsLog.put("Number of regions", worldRegions.getRegions().size());
             LogListBlock globalRegionLog = regionsLog.putChild("Global region");
-            
+
             Region globalRegion = worldRegions.getRegion("__global__");
             if (globalRegion == null) {
                 globalRegionLog.put("Status", "UNDEFINED");
@@ -304,17 +299,17 @@ public class ReportWriter {
                                 globalRegion.getFlag(flag));
                     }
                 }
-            }
+            }*/
         }
 
         append(log);
         appendln();
     }
-    
+
     public void write(File file) throws IOException {
         FileWriter writer = null;
         BufferedWriter out;
-        
+
         try {
             writer = new FileWriter(file);
             out = new BufferedWriter(writer);
@@ -329,7 +324,7 @@ public class ReportWriter {
             }
         }
     }
-    
+
     @Override
     public String toString() {
         return output.toString();
