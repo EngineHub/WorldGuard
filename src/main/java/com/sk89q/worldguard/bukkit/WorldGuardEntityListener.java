0920dc934646147e5aa684095317f9a77358e6b4
wizjany <wizjany@gmail.com>
2013-04-09 17:18:33 -0400

    Apply other-explosion flag to entity damage as well.

--- src/main/java/com/sk89q/worldguard/bukkit/WorldGuardEntityListener.java
+++ src/main/java/com/sk89q/worldguard/bukkit/WorldGuardEntityListener.java
@@ -192,7 +192,19 @@ public class WorldGuardEntityListener implements Listener {
                 return;
             }
 
-            if (wcfg.disableExplosionDamage && type == DamageCause.BLOCK_EXPLOSION) {
+            if (type == DamageCause.BLOCK_EXPLOSION
+                    && (wcfg.disableExplosionDamage || wcfg.blockOtherExplosions
+                            || !plugin.getGlobalRegionManager().allows(DefaultFlag.OTHER_EXPLOSION, player.getLocation()))) {
+                event.setCancelled(true);
+                return;
+            }
+        } else {
+
+            // for whatever reason, plugin-caused explosions with a null entity count as block explosions and aren't
+            // handled anywhere else
+            if (type == DamageCause.BLOCK_EXPLOSION
+                    && (wcfg.blockOtherExplosions
+                            || !plugin.getGlobalRegionManager().allows(DefaultFlag.OTHER_EXPLOSION, defender.getLocation()))) {
                 event.setCancelled(true);
                 return;
             }
