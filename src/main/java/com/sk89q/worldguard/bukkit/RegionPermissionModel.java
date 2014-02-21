62f6cf198e75dde76606ecaecd03f3fe89f11992
NineteenGiraffes <NineteenGiraffes@gmail.com>
2014-02-21 03:11:36 -0500

    combined all region commands into one class
    removed RegionMemberCommands and put cleaned up versions of the
    add/remove - member/owner commands into RegionCommands

--- src/main/java/com/sk89q/worldguard/bukkit/RegionPermissionModel.java
+++ src/main/java/com/sk89q/worldguard/bukkit/RegionPermissionModel.java
@@ -88,6 +88,22 @@ public class RegionPermissionModel extends AbstractPermissionModel {
         return hasPatternPermission("teleport", region);
     }
     
+    public boolean mayAddMember(ProtectedRegion region) {
+        return hasPatternPermission("addmember", region);
+    }
+    
+    public boolean mayAddOwner(ProtectedRegion region) {
+        return hasPatternPermission("addowner", region);
+    }
+    
+    public boolean mayRemoveMember(ProtectedRegion region) {
+        return hasPatternPermission("removemember", region);
+    }
+    
+    public boolean mayRemoveOwner(ProtectedRegion region) {
+        return hasPatternPermission("removeowner", region);
+    }
+    
     public boolean mayList() {
         return hasPluginPermission("region.list");
     }
@@ -105,7 +121,7 @@ public class RegionPermissionModel extends AbstractPermissionModel {
     }
     
     public boolean maySetFlag(ProtectedRegion region) {
-        return hasPatternPermission("flag.regions", region);
+        return hasPatternPermission("flag", region);
     }
     
     public boolean maySetFlag(ProtectedRegion region, Flag<?> flag) {
