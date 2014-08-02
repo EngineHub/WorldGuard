7986238936340a648821e1d4d28059dbc0dcef12
sk89q <the.sk89q@gmail.com>
2014-08-01 21:29:40 -0700

    Add migrations for region MySQL support.

--- src/main/resources/migrations/region/mysql/V1__Initial.sql
+++ src/main/resources/migrations/region/mysql/V1__Initial.sql
@@ -5,7 +5,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
 -- -----------------------------------------------------
 -- Table `group`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `group` (
+CREATE  TABLE IF NOT EXISTS `${tablePrefix}group` (
   `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(64) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
   PRIMARY KEY (`id`) ,
@@ -18,7 +18,7 @@ COLLATE = utf8_bin;
 -- -----------------------------------------------------
 -- Table `world`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `world` (
+CREATE  TABLE IF NOT EXISTS `${tablePrefix}world` (
   `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(128) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
   PRIMARY KEY (`id`) ,
@@ -31,7 +31,7 @@ COLLATE = utf8_bin;
 -- -----------------------------------------------------
 -- Table `region`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `region` (
+CREATE  TABLE IF NOT EXISTS `${tablePrefix}region` (
   `id` VARCHAR(128) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
   `world_id` INT(10) UNSIGNED NOT NULL ,
   `type` ENUM('cuboid','poly2d','global') CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
@@ -40,14 +40,14 @@ CREATE  TABLE IF NOT EXISTS `region` (
   PRIMARY KEY (`id`, `world_id`) ,
   INDEX `fk_region_world` (`world_id` ASC) ,
   INDEX `parent` (`parent` ASC) ,
-  CONSTRAINT `fk_region_world1`
+  CONSTRAINT `fk_${tablePrefix}region_world1`
     FOREIGN KEY (`world_id` )
-    REFERENCES `world` (`id` )
+    REFERENCES `${tablePrefix}world` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT `parent`
+  CONSTRAINT `${tablePrefix}parent`
     FOREIGN KEY (`parent` )
-    REFERENCES `region` (`id` )
+    REFERENCES `${tablePrefix}region` (`id` )
     ON DELETE SET NULL
     ON UPDATE CASCADE)
 ENGINE = InnoDB
@@ -58,7 +58,7 @@ COLLATE = utf8_bin;
 -- -----------------------------------------------------
 -- Table `region_cuboid`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `region_cuboid` (
+CREATE  TABLE IF NOT EXISTS `${tablePrefix}region_cuboid` (
   `region_id` VARCHAR(128) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
   `world_id` INT(10) UNSIGNED NOT NULL ,
   `min_x` BIGINT(20) NOT NULL ,
@@ -69,9 +69,9 @@ CREATE  TABLE IF NOT EXISTS `region_cuboid` (
   `max_z` BIGINT(20) NOT NULL ,
   PRIMARY KEY (`region_id`, `world_id`) ,
   INDEX `fk_region_cuboid_region` (`region_id` ASC) ,
-  CONSTRAINT `fk_region_cuboid_region`
+  CONSTRAINT `fk_${tablePrefix}region_cuboid_region`
     FOREIGN KEY (`region_id` , `world_id` )
-    REFERENCES `region` (`id` , `world_id` )
+    REFERENCES `${tablePrefix}region` (`id` , `world_id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB
@@ -82,7 +82,7 @@ COLLATE = utf8_bin;
 -- -----------------------------------------------------
 -- Table `region_flag`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `region_flag` (
+CREATE  TABLE IF NOT EXISTS `${tablePrefix}region_flag` (
   `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
   `region_id` VARCHAR(128) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
   `world_id` INT(10) UNSIGNED NOT NULL ,
@@ -90,9 +90,9 @@ CREATE  TABLE IF NOT EXISTS `region_flag` (
   `value` VARCHAR(256) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
   PRIMARY KEY (`id`) ,
   INDEX `fk_flags_region` (`region_id` ASC, `world_id` ASC) ,
-  CONSTRAINT `fk_flags_region`
+  CONSTRAINT `fk_${tablePrefix}flags_region`
     FOREIGN KEY (`region_id` , `world_id` )
-    REFERENCES `region` (`id` , `world_id` )
+    REFERENCES `${tablePrefix}region` (`id` , `world_id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB
@@ -103,7 +103,7 @@ COLLATE = utf8_bin;
 -- -----------------------------------------------------
 -- Table `region_groups`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `region_groups` (
+CREATE  TABLE IF NOT EXISTS `${tablePrefix}region_groups` (
   `region_id` VARCHAR(128) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
   `world_id` INT(10) UNSIGNED NOT NULL ,
   `group_id` INT(10) UNSIGNED NOT NULL ,
@@ -111,14 +111,14 @@ CREATE  TABLE IF NOT EXISTS `region_groups` (
   PRIMARY KEY (`region_id`, `world_id`, `group_id`) ,
   INDEX `fk_region_groups_region` (`region_id` ASC) ,
   INDEX `fk_region_groups_group` (`group_id` ASC) ,
-  CONSTRAINT `fk_region_groups_group`
+  CONSTRAINT `fk_${tablePrefix}region_groups_group`
     FOREIGN KEY (`group_id` )
-    REFERENCES `group` (`id` )
+    REFERENCES `${tablePrefix}group` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT `fk_region_groups_region`
+  CONSTRAINT `fk_${tablePrefix}region_groups_region`
     FOREIGN KEY (`region_id` , `world_id` )
-    REFERENCES `region` (`id` , `world_id` )
+    REFERENCES `${tablePrefix}region` (`id` , `world_id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB
@@ -129,7 +129,7 @@ COLLATE = utf8_bin;
 -- -----------------------------------------------------
 -- Table `user`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `user` (
+CREATE  TABLE IF NOT EXISTS `${tablePrefix}user` (
   `id` INT(10) UNSIGNED NOT NULL AUTO_INCREMENT ,
   `name` VARCHAR(64) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
   PRIMARY KEY (`id`) ,
@@ -142,7 +142,7 @@ COLLATE = utf8_bin;
 -- -----------------------------------------------------
 -- Table `region_players`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `region_players` (
+CREATE  TABLE IF NOT EXISTS `${tablePrefix}region_players` (
   `region_id` VARCHAR(128) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
   `world_id` INT(10) UNSIGNED NOT NULL ,
   `user_id` INT(10) UNSIGNED NOT NULL ,
@@ -150,14 +150,14 @@ CREATE  TABLE IF NOT EXISTS `region_players` (
   PRIMARY KEY (`region_id`, `world_id`, `user_id`) ,
   INDEX `fk_region_players_region` (`region_id` ASC) ,
   INDEX `fk_region_users_user` (`user_id` ASC) ,
-  CONSTRAINT `fk_region_users_region`
+  CONSTRAINT `fk_${tablePrefix}region_users_region`
     FOREIGN KEY (`region_id` , `world_id` )
-    REFERENCES `region` (`id` , `world_id` )
+    REFERENCES `${tablePrefix}region` (`id` , `world_id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE,
-  CONSTRAINT `fk_region_users_user`
+  CONSTRAINT `fk_${tablePrefix}region_users_user`
     FOREIGN KEY (`user_id` )
-    REFERENCES `user` (`id` )
+    REFERENCES `${tablePrefix}user` (`id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB
@@ -168,16 +168,16 @@ COLLATE = utf8_bin;
 -- -----------------------------------------------------
 -- Table `region_poly2d`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `region_poly2d` (
+CREATE  TABLE IF NOT EXISTS `${tablePrefix}region_poly2d` (
   `region_id` VARCHAR(128) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
   `world_id` INT(10) UNSIGNED NOT NULL ,
   `min_y` INT(11) NOT NULL ,
   `max_y` INT(11) NOT NULL ,
   PRIMARY KEY (`region_id`, `world_id`) ,
   INDEX `fk_region_poly2d_region` (`region_id` ASC) ,
-  CONSTRAINT `fk_region_poly2d_region`
+  CONSTRAINT `fk_${tablePrefix}region_poly2d_region`
     FOREIGN KEY (`region_id` , `world_id` )
-    REFERENCES `region` (`id` , `world_id` )
+    REFERENCES `${tablePrefix}region` (`id` , `world_id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB
@@ -188,7 +188,7 @@ COLLATE = utf8_bin;
 -- -----------------------------------------------------
 -- Table `region_poly2d_point`
 -- -----------------------------------------------------
-CREATE  TABLE IF NOT EXISTS `region_poly2d_point` (
+CREATE  TABLE IF NOT EXISTS `${tablePrefix}region_poly2d_point` (
   `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT ,
   `region_id` VARCHAR(128) CHARACTER SET 'utf8' COLLATE 'utf8_bin' NOT NULL ,
   `world_id` INT(10) UNSIGNED NOT NULL ,
@@ -196,9 +196,9 @@ CREATE  TABLE IF NOT EXISTS `region_poly2d_point` (
   `z` BIGINT(20) NOT NULL ,
   PRIMARY KEY (`id`) ,
   INDEX `fk_region_poly2d_point_region_poly2d` (`region_id` ASC, `world_id` ASC) ,
-  CONSTRAINT `fk_region_poly2d_point_region_poly2d`
+  CONSTRAINT `fk_${tablePrefix}region_poly2d_point_region_poly2d`
     FOREIGN KEY (`region_id` , `world_id` )
-    REFERENCES `region_poly2d` (`region_id` , `world_id` )
+    REFERENCES `${tablePrefix}region_poly2d` (`region_id` , `world_id` )
     ON DELETE CASCADE
     ON UPDATE CASCADE)
 ENGINE = InnoDB
