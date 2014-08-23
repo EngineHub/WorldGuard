a6118c3aa4b808ddca96fd1d276aff94105faf00
sk89q <the.sk89q@gmail.com>
2014-08-22 17:30:18 -0700

    Unify debouncing method.

--- src/main/java/com/sk89q/worldguard/bukkit/listener/debounce/AbstractEventDebounce.java
+++ src/main/java/com/sk89q/worldguard/bukkit/listener/debounce/AbstractEventDebounce.java
@@ -17,37 +17,38 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-package com.sk89q.worldguard.bukkit.listener;
+package com.sk89q.worldguard.bukkit.listener.debounce;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
 import com.sk89q.worldguard.bukkit.util.Events;
-import com.sk89q.worldguard.bukkit.util.WGMetadata;
 import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
-import org.bukkit.metadata.Metadatable;
 
-class EventDebounce {
+import java.util.concurrent.TimeUnit;
 
-    private final long time = System.currentTimeMillis();
-    private final int debounceTime;
-    private final boolean cancelled;
+public class AbstractEventDebounce<K> {
 
-    private EventDebounce(int debounceTime, boolean cancelled) {
-        this.debounceTime = debounceTime;
-        this.cancelled = cancelled;
+    private final Cache<K, Entry> cache;
+
+    AbstractEventDebounce(int debounceTime) {
+        cache = CacheBuilder.newBuilder()
+                .maximumSize(1000)
+                .expireAfterWrite(debounceTime, TimeUnit.MILLISECONDS)
+                .concurrencyLevel(2)
+                .build(new CacheLoader<K, Entry>() {
+                    @Override
+                    public Entry load(K key) throws Exception {
+                        return new Entry();
+                    }
+                });
     }
 
-    public boolean isValid() {
-        return System.currentTimeMillis() - time < debounceTime;
-    }
-
-    public boolean getLastCancellation() {
-        return cancelled;
-    }
-
-    public static <T extends Event & Cancellable> void debounce(Metadatable target, String key, int debounceTime, Cancellable originalEvent, T firedEvent) {
-        EventDebounce debounce = WGMetadata.getIfPresent(target, key, EventDebounce.class);
-        if (debounce != null && debounce.isValid()) {
-            if (debounce.getLastCancellation()) {
+    protected <T extends Event & Cancellable> void debounce(K key, Cancellable originalEvent, T firedEvent) {
+        Entry entry = cache.getUnchecked(key);
+        if (entry.cancelled != null) {
+            if (entry.cancelled) {
                 originalEvent.setCancelled(true);
             }
         } else {
@@ -55,8 +56,12 @@ class EventDebounce {
             if (cancelled) {
                 originalEvent.setCancelled(true);
             }
-            WGMetadata.put(target, key, new EventDebounce(debounceTime, cancelled));
+            entry.cancelled = cancelled;
         }
+    }
+
+    private static class Entry {
+        private Boolean cancelled;
     }
 
 }
