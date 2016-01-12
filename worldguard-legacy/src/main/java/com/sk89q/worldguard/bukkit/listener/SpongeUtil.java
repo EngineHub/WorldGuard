311732a1e57849a913dffd1ff2556c422fcd1a10
Rae <huntergplays@gmail.com>
2016-01-12 23:33:58 +0000

    Added lava support

--- worldguard-legacy/src/main/java/com/sk89q/worldguard/bukkit/listener/SpongeUtil.java
+++ worldguard-legacy/src/main/java/com/sk89q/worldguard/bukkit/listener/SpongeUtil.java
@@ -25,7 +25,9 @@ import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
 import org.bukkit.World;
 
 import static com.sk89q.worldguard.bukkit.BukkitUtil.isBlockWater;
+import static com.sk89q.worldguard.bukkit.BukkitUtil.isBlockLava;
 import static com.sk89q.worldguard.bukkit.BukkitUtil.setBlockToWater;
+import static com.sk89q.worldguard.bukkit.BukkitUtil.setBlockToLava;
 
 public final class SpongeUtil {
 
@@ -49,6 +51,30 @@ public final class SpongeUtil {
             for (int cy = -wcfg.spongeRadius; cy <= wcfg.spongeRadius; cy++) {
                 for (int cz = -wcfg.spongeRadius; cz <= wcfg.spongeRadius; cz++) {
                     if (isBlockWater(world, ox + cx, oy + cy, oz + cz)) {
+                        world.getBlockAt(ox + cx, oy + cy, oz + cz).setTypeId(0);
+                    }
+                }
+            }
+        }
+    }
+    
+    /**
+     * Remove lava around a sponge.
+     *
+     * @param plugin The plugin instace
+     * @param world The world the sponge isin
+     * @param ox The x coordinate of the 'sponge' block
+     * @param oy The y coordinate of the 'sponge' block
+     * @param oz The z coordinate of the 'sponge' block
+     */
+    public static void clearSpongeLava(WorldGuardPlugin plugin, World world, int ox, int oy, int oz) {
+        ConfigurationManager cfg = plugin.getGlobalStateManager();
+        WorldConfiguration wcfg = cfg.get(world);
+
+        for (int cx = -wcfg.spongeRadius; cx <= wcfg.spongeRadius; cx++) {
+            for (int cy = -wcfg.spongeRadius; cy <= wcfg.spongeRadius; cy++) {
+                for (int cz = -wcfg.spongeRadius; cz <= wcfg.spongeRadius; cz++) {
+                    if (isBlockLava(world, ox + cx, oy + cy, oz + cz)) {
                         world.getBlockAt(ox + cx, oy + cy, oz + cz).setTypeId(0);
                     }
                 }
@@ -125,6 +151,80 @@ public final class SpongeUtil {
             for (cy = oy - wcfg.spongeRadius - 1; cy <= oy + wcfg.spongeRadius + 1; cy++) {
                 if (isBlockWater(world, cx, cy, cz)) {
                     setBlockToWater(world, cx, cy, cz - 1);
+                }
+            }
+        }
+    }
+    
+    /**
+     * Add lava around a sponge.
+     * 
+     * @param plugin The plugin instance
+     * @param world The world the sponge is located in
+     * @param ox The x coordinate of the 'sponge' block
+     * @param oy The y coordinate of the 'sponge' block
+     * @param oz The z coordinate of the 'sponge' block
+     */
+    public static void addSpongeLava(WorldGuardPlugin plugin, World world, int ox, int oy, int oz) {
+        ConfigurationManager cfg = plugin.getGlobalStateManager();
+        WorldConfiguration wcfg = cfg.get(world);
+
+        // The negative x edge
+        int cx = ox - wcfg.spongeRadius - 1;
+        for (int cy = oy - wcfg.spongeRadius - 1; cy <= oy + wcfg.spongeRadius + 1; cy++) {
+            for (int cz = oz - wcfg.spongeRadius - 1; cz <= oz + wcfg.spongeRadius + 1; cz++) {
+                if (isBlockLava(world, cx, cy, cz)) {
+                    setBlockToLava(world, cx + 1, cy, cz);
+                }
+            }
+        }
+
+        // The positive x edge
+        cx = ox + wcfg.spongeRadius + 1;
+        for (int cy = oy - wcfg.spongeRadius - 1; cy <= oy + wcfg.spongeRadius + 1; cy++) {
+            for (int cz = oz - wcfg.spongeRadius - 1; cz <= oz + wcfg.spongeRadius + 1; cz++) {
+                if (isBlockLava(world, cx, cy, cz)) {
+                    setBlockToLava(world, cx - 1, cy, cz);
+                }
+            }
+        }
+
+        // The negative y edge
+        int cy = oy - wcfg.spongeRadius - 1;
+        for (cx = ox - wcfg.spongeRadius - 1; cx <= ox + wcfg.spongeRadius + 1; cx++) {
+            for (int cz = oz - wcfg.spongeRadius - 1; cz <= oz + wcfg.spongeRadius + 1; cz++) {
+                if (isBlockLava(world, cx, cy, cz)) {
+                    setBlockToLava(world, cx, cy + 1, cz);
+                }
+            }
+        }
+
+        // The positive y edge
+        cy = oy + wcfg.spongeRadius + 1;
+        for (cx = ox - wcfg.spongeRadius - 1; cx <= ox + wcfg.spongeRadius + 1; cx++) {
+            for (int cz = oz - wcfg.spongeRadius - 1; cz <= oz + wcfg.spongeRadius + 1; cz++) {
+                if (isBlockLava(world, cx, cy, cz)) {
+                    setBlockToLava(world, cx, cy - 1, cz);
+                }
+            }
+        }
+
+        // The negative z edge
+        int cz = oz - wcfg.spongeRadius - 1;
+        for (cx = ox - wcfg.spongeRadius - 1; cx <= ox + wcfg.spongeRadius + 1; cx++) {
+            for (cy = oy - wcfg.spongeRadius - 1; cy <= oy + wcfg.spongeRadius + 1; cy++) {
+                if (isBlockLava(world, cx, cy, cz)) {
+                    setBlockToLava(world, cx, cy, cz + 1);
+                }
+            }
+        }
+
+        // The positive z edge
+        cz = oz + wcfg.spongeRadius + 1;
+        for (cx = ox - wcfg.spongeRadius - 1; cx <= ox + wcfg.spongeRadius + 1; cx++) {
+            for (cy = oy - wcfg.spongeRadius - 1; cy <= oy + wcfg.spongeRadius + 1; cy++) {
+                if (isBlockLava(world, cx, cy, cz)) {
+                    setBlockToLava(world, cx, cy, cz - 1);
                 }
             }
         }
