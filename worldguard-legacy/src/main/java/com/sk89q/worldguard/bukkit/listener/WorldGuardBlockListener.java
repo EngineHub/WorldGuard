13f899f608e5d3c6675d3ccf6b7e0af437bf8912
wizjany <wizjany@gmail.com>
2016-08-13 16:57:26 -0400

    Add frosted ice to ice-melt flag. Fixes WORLDGUARD-3745.

--- worldguard-legacy/src/main/java/com/sk89q/worldguard/bukkit/listener/WorldGuardBlockListener.java
+++ worldguard-legacy/src/main/java/com/sk89q/worldguard/bukkit/listener/WorldGuardBlockListener.java
@@ -624,6 +624,7 @@ public class WorldGuardBlockListener implements Listener {
 
         switch (event.getBlock().getTypeId()) {
         case BlockID.ICE:
+        case BlockID.FROSTED_ICE:
             if (wcfg.disableIceMelting) {
                 event.setCancelled(true);
                 return;
