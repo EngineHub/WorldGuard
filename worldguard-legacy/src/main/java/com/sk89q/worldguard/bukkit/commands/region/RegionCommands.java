f6c2e426287230958f6745029ec2fccc8e3b98e7
Benedikt Hruschka <BenediktHr@icloud.com>
2017-05-27 20:35:26 +0200

    Remove unused imports

--- worldguard-legacy/src/main/java/com/sk89q/worldguard/bukkit/commands/region/RegionCommands.java
+++ worldguard-legacy/src/main/java/com/sk89q/worldguard/bukkit/commands/region/RegionCommands.java
@@ -71,8 +71,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
