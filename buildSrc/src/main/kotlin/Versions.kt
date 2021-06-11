9d3e7869e58323f86097603f2f5367dcda930ecc
JOO200 <github@joo200.de>
2021-06-11 17:52:43 +0200

    Updated dependencies and gradle

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
 //    const val PISTON = "0.4.3"
 //    const val AUTO_VALUE = "1.6.5"
-    const val WORLDEDIT = "7.2.0-SNAPSHOT"
+    const val WORLDEDIT = "7.2.6-SNAPSHOT"
     const val JUNIT = "4.11"
     const val SQUIRRELID = "0.2.0"
     const val GUAVA = "21.0"
--- worldguard-core/build.gradle.kts
+++ worldguard-core/build.gradle.kts
@@ -10,7 +10,7 @@ dependencies {
     "api"(project(":worldguard-libs:core"))
     "api"("com.sk89q.worldedit:worldedit-core:${Versions.WORLDEDIT}")
     "implementation"("org.flywaydb:flyway-core:3.0")
-    "implementation"("org.yaml:snakeyaml:1.9")
+    "implementation"("org.yaml:snakeyaml:1.29")
     "implementation"("com.google.guava:guava:${Versions.GUAVA}")
 
     "compileOnly"("com.google.code.findbugs:jsr305:1.3.9")
