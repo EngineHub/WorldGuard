fb9705e4f093cb08d60fd294a8788b4cf1f8d782
wizjany <wizjany@gmail.com>
2021-11-25 11:27:00 -0500

    Update gradle.
    Getting ready for java 17.

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,7 +10,7 @@ repositories {
 
 dependencies {
     implementation(gradleApi())
-    implementation("gradle.plugin.org.cadixdev.gradle:licenser:0.6.0")
+    implementation("gradle.plugin.org.cadixdev.gradle:licenser:0.6.1")
     implementation("org.ajoberstar.grgit:grgit-gradle:4.1.0")
     implementation("com.github.jengelman.gradle.plugins:shadow:6.1.0")
     implementation("org.jfrog.buildinfo:build-info-extractor-gradle:4.21.0")
