703146bc1879d0c181a088b1a6b7e4c81593a6a2
wizjany <wizjany@gmail.com>
2021-11-30 20:42:54 -0500

    Update guava to match MC 1.18 guava.

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,9 +1,9 @@
 object Versions {
 //    const val PISTON = "0.4.3"
 //    const val AUTO_VALUE = "1.6.5"
-    const val WORLDEDIT = "7.2.6-SNAPSHOT"
+    const val WORLDEDIT = "7.2.7"
     const val JUNIT = "5.7.0"
     const val SQUIRRELID = "0.3.0"
-    const val GUAVA = "21.0"
+    const val GUAVA = "31.0.1-jre"
     const val MOCKITO = "3.7.7"
 }
