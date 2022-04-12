81f7d9c18d98698fca76213d5cc0443d7448b281
r4nx <mod34@yandex.ru>
2022-04-12 13:47:07 +0300

    Use fixed SquirrelID version

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -4,7 +4,7 @@ object Versions {
     const val AUTO_VALUE = "1.9"
     const val JUNIT = "5.8.1"
     const val MOCKITO = "4.3.1"
-    const val SQUIRRELID = "0.3.0"
+    const val SQUIRRELID = "0.4.0"
     const val GUAVA = "31.0.1-jre"
     const val FINDBUGS = "3.0.2"
 }
