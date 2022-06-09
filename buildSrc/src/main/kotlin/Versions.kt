23521858fd5338de86808a4d60b191a1ee89c619
JOO200 <github@joo200.de>
2022-06-09 20:14:18 +0200

    Bumped versions: Minecraft to 1.19 and JDK 17

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,7 +1,7 @@
 object Versions {
 //    const val PISTON = "0.4.3"
 //    const val AUTO_VALUE = "1.6.5"
-    const val WORLDEDIT = "7.2.7"
+    const val WORLDEDIT = "7.2.10"
     const val JUNIT = "5.7.0"
     const val SQUIRRELID = "0.3.0"
     const val GUAVA = "31.0.1-jre"
