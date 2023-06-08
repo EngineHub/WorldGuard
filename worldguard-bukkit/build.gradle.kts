16887c85403bad9c5eaed8f75b09523ebc3215ae
Joo200 <github@joo200.de>
2023-06-08 20:55:11 +0200

    deps: Update Paper to 1.20, snakeyml to 2.0

--- worldguard-bukkit/build.gradle.kts
+++ worldguard-bukkit/build.gradle.kts
@@ -20,8 +20,8 @@ configurations {
 
 dependencies {
     "api"(project(":worldguard-core"))
-    "compileOnly"("io.papermc.paper:paper-api:1.19.3-R0.1-SNAPSHOT")
-    "runtimeOnly"("org.spigotmc:spigot-api:1.19.3-R0.1-SNAPSHOT") {
+    "compileOnly"("io.papermc.paper:paper-api:1.20-R0.1-SNAPSHOT")
+    "runtimeOnly"("org.spigotmc:spigot-api:1.20-R0.1-SNAPSHOT") {
         exclude("junit", "junit")
     }
     "api"("com.sk89q.worldedit:worldedit-bukkit:${Versions.WORLDEDIT}") { isTransitive = false }
