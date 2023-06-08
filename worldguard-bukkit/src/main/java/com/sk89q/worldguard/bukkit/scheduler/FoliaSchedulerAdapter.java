e632541eb309c17b432178eea4723381df8862bd
HarvelsX <90945793+HarvelsX@users.noreply.github.com>
2023-06-09 01:38:22 +0300

    Add scheduler adapter interface & implementation for: folia, bukkit;
    (cherry picked from commit a6f859964023765bcfa5299fa84e25443a384bd9)

--- worldguard-bukkit/src/main/java/com/sk89q/worldguard/bukkit/scheduler/FoliaSchedulerAdapter.java
+++ worldguard-bukkit/src/main/java/com/sk89q/worldguard/bukkit/scheduler/FoliaSchedulerAdapter.java
@@ -1,19 +1,86 @@
+/*
+ * WorldGuard, a suite of tools for Minecraft
+ * Copyright (C) sk89q <http://www.sk89q.com>
+ * Copyright (C) WorldGuard team and contributors
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.sk89q.worldguard.bukkit.scheduler;
 
-public class FoliaSchedulerAdapter {
+import io.papermc.paper.threadedregions.scheduler.AsyncScheduler;
+import io.papermc.paper.threadedregions.scheduler.RegionScheduler;
+import org.bukkit.Location;
+import org.bukkit.entity.Entity;
+import org.bukkit.plugin.Plugin;
 
-  private static final boolean SUPPORTED = checkSupport();
+import java.util.concurrent.TimeUnit;
 
-  public static boolean isSupported() {
-    return SUPPORTED;
-  }
+public class FoliaSchedulerAdapter implements SchedulerAdapter {
+    private static final boolean SUPPORTED = checkSupport();
 
-  private static boolean checkSupport() {
-    try {
-      Class.forName("io.papermc.paper.threadedregions.RegionizedServer");
-      return true;
-    } catch (ClassNotFoundException e) {
-      return false;
+    private final Plugin plugin;
+    private final AsyncScheduler asyncScheduler;
+    private final RegionScheduler regionScheduler;
+
+    public FoliaSchedulerAdapter(final Plugin plugin) {
+        this.plugin = plugin;
+        this.asyncScheduler = plugin.getServer().getAsyncScheduler();
+        this.regionScheduler = plugin.getServer().getRegionScheduler();
     }
-  }
+
+    public static boolean isSupported() {
+        return SUPPORTED;
+    }
+
+    private static boolean checkSupport() {
+        try {
+            Class.forName("io.papermc.paper.threadedregions.RegionizedServer");
+            return true;
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
+    }
+
+    @Override
+    public void runAsync(final Runnable runnable) {
+        asyncScheduler.runNow(plugin, task -> runnable.run());
+    }
+
+    @Override
+    public void runAsyncRate(final Runnable runnable, final long delay, final long period) {
+        asyncScheduler.runAtFixedRate(plugin, task -> runnable.run(), delay * 50, period * 50, TimeUnit.MILLISECONDS);
+    }
+
+    @Override
+    public void runAtEntity(final Entity entity, final Runnable runnable) {
+        entity.getScheduler().run(plugin, task -> runnable.run(), null);
+    }
+
+    @Override
+    public void runAtEntityLater(final Entity entity, final Runnable runnable, final long delay) {
+        entity.getScheduler().execute(plugin, runnable, null, delay);
+    }
+
+    @Override
+    public void runAtRegion(final Location location, final Runnable runnable) {
+        regionScheduler.execute(plugin, location, runnable);
+    }
+
+    @Override
+    public void cancelTasks() {
+        asyncScheduler.cancelTasks(plugin);
+    }
+
 }
