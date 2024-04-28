da3c81a88dd47bf16faec47263c72d06007dac58
Joo200 <github@joo200.de>
2024-04-28 15:32:32 +0200

    Update dependencies to 1.20.5 and jdk 21

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -11,7 +11,19 @@ repositories {
 dependencies {
     implementation(gradleApi())
     implementation("gradle.plugin.org.cadixdev.gradle:licenser:0.6.1")
-    implementation("org.ajoberstar.grgit:grgit-gradle:4.1.1")
-    implementation("gradle.plugin.com.github.johnrengelman:shadow:7.1.2")
-    implementation("org.jfrog.buildinfo:build-info-extractor-gradle:4.27.1")
+    implementation("org.ajoberstar.grgit:grgit-gradle:5.2.2")
+    implementation("com.github.johnrengelman:shadow:8.1.1")
+    implementation("org.jfrog.buildinfo:build-info-extractor-gradle:5.2.0")
+    constraints {
+        val asmVersion = "[9.7,)"
+        implementation("org.ow2.asm:asm:$asmVersion") {
+            because("Need Java 21 support in shadow")
+        }
+        implementation("org.ow2.asm:asm-commons:$asmVersion") {
+            because("Need Java 21 support in shadow")
+        }
+        implementation("org.vafer:jdependency:[2.10,)") {
+            because("Need Java 21 support in shadow")
+        }
+    }
 }
