c6fbfb87a377d624c5cf14d6fa46e543799949b1
CyberFlame <cyberflameu@gmail.com>
2024-07-05 02:21:57 +1200

    remove double space

--- worldguard-core/src/main/java/com/sk89q/worldguard/session/Session.java
+++ worldguard-core/src/main/java/com/sk89q/worldguard/session/Session.java
@@ -112,7 +112,7 @@ public class Session {
         disableBypass = cfg.disableDefaultBypass;
         if (cfg.announceBypassStatus && player.hasPermission("worldguard.region.toggle-bypass")) {
             player.printInfo(TextComponent.of(
-                    "You are " + (disableBypass ? "not" : "") + " bypassing region protection. " +
+                    "You are " + (disableBypass ? "not " : "") + "bypassing region protection. " +
                     "You can toggle this with /rg bypass", TextColor.DARK_PURPLE));
         }
 
