7cb9473d3a42f1be07312cca810ee103c934190d
HPnetwork <hpnetwork@qq.com>
2026-09-18 08:53:49 -0400

    Fix region flag clear feedback

--- worldguard-core/src/main/java/com/sk89q/worldguard/commands/region/RegionCommands.java
+++ worldguard-core/src/main/java/com/sk89q/worldguard/commands/region/RegionCommands.java
@@ -597,7 +597,8 @@ public final class RegionCommands extends RegionCommandsBase {
         if (value != null) {
             // Set the flag if [value] was given even if [-g group] was given as well
             try {
-                value = setFlag(existing, foundFlag, sender, value).toString();
+                Object parsedValue = setFlag(existing, foundFlag, sender, value);
+                value = parsedValue == null ? "none" : parsedValue.toString();
             } catch (InvalidFlagFormat e) {
                 throw new CommandException(e.getMessage());
             }
